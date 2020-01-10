3b17732036d06cb0e121ee5eed979f6cf5163669
杨涛 <tyang@dfocuspace.com>
2020-01-10 15:26:05 +0800

    feat:mysql

--- python3/__init__.py
+++
@@ -1,2 +0,0 @@
-import pymysql
-db = pymysql.connect("127.0.0.1", "root", "123456", "python3", port=3306, charset='utf8')
--- python3/settings.py
+++ python3/settings.py
@@ -79,9 +79,13 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    # mysql配置
     'default': {
-        'ENGINE': None,
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python3',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
